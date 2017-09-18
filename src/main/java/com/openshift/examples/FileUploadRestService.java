23770e872ec6c5a05f42c47de09971be7e1d1b01
okori <32044294+okori@users.noreply.github.com>
2017-09-18 08:59:09 -0700

    Update FileUploadRestService.java

--- src/main/java/com/openshift/examples/FileUploadRestService.java
+++ src/main/java/com/openshift/examples/FileUploadRestService.java
@@ -1,62 +1,33 @@
 package com.openshift.examples;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+ 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataParam;
-
-@Path("/file")
-public class FileUploadRestService {
-	
-	
-	@POST
-	@Path("/upload")
-	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(
-			@FormDataParam("file") InputStream uploadedInputStream,
-			@FormDataParam("file") FormDataContentDisposition fileDetail) {
-
-		String uploadedFileLocation = System.getenv("OPENSHIFT_DATA_DIR");
-		// save it
-		writeToFile(uploadedInputStream, uploadedFileLocation,fileDetail);
-
-		String output = "File uploaded to : " + uploadedFileLocation;
-
-		return Response.status(200).entity(output).build();
-
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+ 
+@Path("/ctofservice")
+public class CtoFService {
+	@GET
+	@Produces("application/xml")
+	public String convertCtoF() {
+ 
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
 	}
-
-	private void writeToFile(InputStream uploadedInputStream,
-			String uploadedFileLocation, FormDataContentDisposition fileDetail) {
-
-		try {
-			String filePath = uploadedFileLocation + fileDetail.getFileName();
-			OutputStream out = new FileOutputStream(new File(
-					filePath));
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			out = new FileOutputStream(new File(filePath));
-			while ((read = uploadedInputStream.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
-			}
-			out.flush();
-			out.close();
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-
+ 
+	@Path("{c}")
+	@GET
+	@Produces("application/xml")
+	public String convertCtoFfromInput(@PathParam("c") Double c) {
+		Double fahrenheit;
+		Double celsius = c;
+		fahrenheit = ((celsius * 9) / 5) + 32;
+ 
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
 	}
-
 }
