21a33bc09b2d84a88159d53af10986a486ba1999
okori <32044294+okori@users.noreply.github.com>
2017-10-06 05:20:48 -0700

    Update badge.java

--- src/main/java/com/openshift/examples/badge.java
+++ src/main/java/com/openshift/examples/badge.java
@@ -15,19 +15,7 @@ public class badge {
 	public String report() {
 		
 		boolean saleStatus = assist.checkSale();
- 
-		//if sale is running and last time it checked sale wasn't running -> send alert
-		if(saleStatus && assist.checkDB()) {
-			assist.sendAlert();
-		}
-		
-		//log to DB whether a sale is running or not
-		if(saleStatus) {
-			assist.logStatus(0); //a sale is running
-		} else {
-			assist.logStatus(1); //no sale running
-		}
-		return "<service>" + "success!" + "</service>";
+		return "<isRunning>" + saleStatus + "</isRunning>";
 	}
   
 }
